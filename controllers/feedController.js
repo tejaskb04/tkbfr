b99659c541722f2d2d8d2d852ba435a416fb6bb7
Shankar Bharadwaj <Shankar Bharadwaj>
2017-05-31 00:08:32 -0700

    instructions for the list of feeds

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -4,6 +4,14 @@ exports.read = function(req, res, next) {
 };
 
 exports.list = function(req, res, next) {
+    /* TODO
+    - create an object called feedsViewModel
+    - create a list of items in this object
+    - an item should have a title and a URL
+    - these items are the feed information stored by the user
+    - for now, hard code the list of items
+    - pass in this view model object as the parameter to the list page
+    */
     res.render("./feed/list", {title: "List Page" });
 };
 
