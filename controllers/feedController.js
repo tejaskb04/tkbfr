a3700dd4ec1f407998caaa40d0d688d589f7740e
tejaskb04 <tejaskb@gmail.com>
2017-06-01 23:11:47 -0700

    display json object

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -1,9 +1,13 @@
 // define routes for feed controller methods
+var express = require("express");
+var app = express;
+
 exports.read = function(req, res, next) {
   res.render("./feed/read", { title: "Read Page" });
 };
 
 exports.list = function(req, res, next) {
+
     /* TODO
     - create an object called feedsViewModel
     - create a list of items in this object
@@ -12,6 +16,15 @@ exports.list = function(req, res, next) {
     - for now, hard code the list of items
     - pass in this view model object as the parameter to the list page
     */
-    res.render("./feed/list", {title: "List Page" });
+
+    var items = [];
+    items.push({title: "BBC Feed", url: "http://feeds.bbci.co.uk/news/rss.xml"});
+    items.push({title: "Sky Feed", url: "http://news.sky.com/feeds/rss/home.xml"});
+    items.push({title: "Techmeme Feed", url: "http://www.techmeme.com/feed.xml"});
+    
+    var viewModel = {};
+    viewModel.title = "List of Feeds";
+    viewModel.items = items;
+    res.render("./feed/list", {viewModel: viewModel});
 };
 
--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-app.use("/static", express.static(__dirname + "public"));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 module.exports.router = express.Router();
 
