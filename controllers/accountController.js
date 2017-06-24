dbbc17a3da717b3fcd70270a4bacea60ddfd7963
tejaskb04 <tejaskb@gmail.com>
2017-06-23 18:46:25 -0700

    trying to get login and register data

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,9 +1,36 @@
 // account controller actions
+var qs = require("querystring");
 
 exports.login = function (req, res, next) {
 	res.render("./account/login", { title: "Login Page" });
 };
 
+exports.loginData = function(req, res, next) {
+	if (req.method == "POST") {
+		var body = "";
+		req.on("data", function(data) {
+			body += data;
+		});
+		req.on("end", function() {
+			var POST = qs.parse(body);
+			// do stuff
+		});
+	}
+}
+
 exports.register = function (req, res, next) {
 	res.render("./account/register", { title: "Register Page" });
 };
+
+exports.registerData = function(req, res, next) {
+	if (req.method == "POST") {
+		var body = "";
+		req.on("data", function(data) {
+			body += data;
+		});
+		req.on("end", function() {
+			var POST = qs.parse(body);
+			// do stuff
+		});
+	}
+}
--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -7,8 +7,10 @@ var accountController = require("../controllers/accountController");
 
 // get login
 router.get("/login", accountController.login);
+router.post("/login", accountController.loginData);
 
 // get register
 router.get("/register", accountController.register);
+router.post("/register", accountController.registerData);
 
 module.exports = router;
